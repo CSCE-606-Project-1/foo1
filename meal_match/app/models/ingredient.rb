103925064b599b8b7d3c74f1d0bd0e480d57b037
Eshan Kelkar <eshankelkar@galorithm.com>
2025-09-20 13:36:53 -0500

    Ensure provider id is unique given a provider name
    Validations have been added in rails model as well
    as the database schema (to avoid race conditions,
    e.g. two users who succeed the rails validation at
    the same time as they don't violate anything at that
    point, but trying to modify db which should fail for
    at least one of them)

--- meal_match/app/models/ingredient.rb
+++ meal_match/app/models/ingredient.rb
@@ -2,4 +2,11 @@ class Ingredient < ApplicationRecord
   validates :provider_name, presence: true
   validates :provider_id, presence: true
   validates :title, presence: true
+
+  # Provider id (i.e of an ingredient stored/given by the provider) should
+  # be unique for a given provider (Provider name)
+  validates :provider_id, uniqueness: {
+                            scope: :provider_name,
+                            message: " must be unique within provider_name"
+                          }
 end
