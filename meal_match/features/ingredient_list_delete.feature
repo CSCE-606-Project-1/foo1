2c66ef526317345c9c9efeb6ad86ed775a4f9a90
Eshan Kelkar <eshankelkar@galorithm.com>
2025-10-05 20:33:30 -0500

    Add acceptance tests for ingredient list delete
    Add cucumber acceptance for verifying the ingredient
    list delete feature. Also ensure that when no ingredient
    list is present (all lists deleted), the ingredient list
    manager shows no table (not even table headers) of
    ingredient lists.

---
+++ meal_match/features/ingredient_list_delete.feature
@@ -0,0 +1,15 @@
+Feature: Delete Ingredient List
+
+  Scenario: Successful deletion of ingredient list
+    Given I am a logged in user
+    And I have already created an ingredient list titled "List 1"
+    And I am on the ingredient lists manager page
+    When I click to delete the ingredient list titled "List 1"
+    Then I should no longer see the ingredient list titled "List 1" in the ingredient lists table
+
+  Scenario: All lists have been deleted
+    Given I am a logged in user
+    And I have already created a few ingredient lists
+    And I am on the ingredient lists manager page
+    When I click to delete all ingredient lists
+    Then I should not see a table of ingredient lists
--- meal_match/features/step_definitions/ingredient_lists_manager_steps.rb
+++ meal_match/features/step_definitions/ingredient_lists_manager_steps.rb
@@ -1,8 +1,18 @@
 CREATE_INGREDIENT_LIST_BTN_LABEL = "Create new list"
+INGREDIENT_LISTS_TABLE_BODY_ID = "ingredient-lists-table-body"
+DELETE_INGREDIENT_LIST_BTN_LABEL = "Delete"
+TABLE_BODY_CSS = "[data-testid='#{INGREDIENT_LISTS_TABLE_BODY_ID}']"
+INGREDIENT_LISTS_TABLE_ID = "ingredient-lists-table"
 
 def ingredient_lists_table_last_row
-  within("[data-testid='ingredient-lists-table-body']") do
+  within(TABLE_BODY_CSS) do
     all("tr").last
+  end
+end
+
+def ingredient_lists_table_row_with_title(title)
+  within(TABLE_BODY_CSS) do
+    find("tr", text: title)
   end
 end
 
@@ -18,6 +28,11 @@ end
 
 When("I click to create a new ingredient list") do
   click_button CREATE_INGREDIENT_LIST_BTN_LABEL
+end
+
+Given("I have already created an ingredient list titled {string}") do |title|
+  raise "Expected a logged in user, but was nil !" if @logged_in_user.nil?
+  IngredientList.create!(user: @logged_in_user, title: title)
 end
 
 Then("I should see the newly created ingredient list below the previously created lists") do
@@ -42,4 +57,39 @@ Then("I should see the newly created list with the default title {string}") do |
 
   first_cell = last_row.all("td").first
   expect(first_cell).to have_content(default_title)
+end
+
+When("I click to delete the ingredient list titled {string}") do |title|
+  row = ingredient_lists_table_row_with_title(title)
+  row.click_button(DELETE_INGREDIENT_LIST_BTN_LABEL)
+end
+
+Then("I should no longer see the ingredient list titled {string} in the ingredient lists table") do |title|
+  # No table body present if there are 0 lists, hence the check is
+  # necessary before trying to search within table. (When there is no
+  # table, then definitely there is no ingredient list in it, so if
+  # has_css? is false then also we are happy.
+  if has_css?(TABLE_BODY_CSS)
+    within(TABLE_BODY_CSS) do
+      expect(page).not_to have_content(title)
+    end
+  end
+end
+
+When("I click to delete all ingredient lists") do
+  # No table body present if there are 0 lists, hence the check is
+  # necessary before trying to delete each ingredient list.
+  while has_css?(TABLE_BODY_CSS)
+    within(TABLE_BODY_CSS) do
+      # We can't iterate over all rows here and press delete, since
+      # one delete press reloads the page causing the html corresponding to
+      # the other rows we have to be in a stale state, so do it one by one.
+      first("tr").click_button(DELETE_INGREDIENT_LIST_BTN_LABEL)
+    end
+  end
+end
+
+Then('I should not see a table of ingredient lists') do
+  save_and_open_page
+  expect(page).not_to have_css("[data-testid='#{INGREDIENT_LISTS_TABLE_ID}']")
 end
